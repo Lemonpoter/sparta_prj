c0b159d964350abacf6bc36ab27c49383a22183a
Do Yeon <153525460+Lemonpoter@users.noreply.github.com>
2024-03-20 17:13:02 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -1,10 +1,14 @@
 def solution(arr,n):
-    for idx,val in enumerate(arr):
+    answer = []
+    for a in range(len(arr)):
         if len(arr) % 2 == 1:
-            if idx % 2 == 0:
-                arr[idx] += n
-        else:
-            if idx % 2 == 1:
-                arr[idx] += n
-    return arr
-
+            if a % 2 == 0:
+                answer.append(arr[a]+n)
+            else:
+                answer.append(arr[a])
+        elif len(arr) % 2 == 0:
+            if a % 2 == 1:
+                answer.append(arr[a]+n)
+            else:
+                answer.append(arr[a])
+    return answer
